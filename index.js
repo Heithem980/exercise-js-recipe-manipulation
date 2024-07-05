fa3b7420f690d0b68d3c5228ca107e88d1457cd1
Heithem980 <heithem.gha98@gmail.com>
2024-07-05 13:27:18 +0200

    WOP

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+//  1.  What is the name of the recipe?
+
+const recipeName = document.getElementById("recipe-name");
+
+console.log(recipeName.innerText);
+
+
+//  2.  What HTML tag is used to display the Recipe name?
+
+console.log(recipeName.tagName)
+
+
+
+
+//  3.  What is the font size of the paragraph tag with the class "description".
+
+
+
+
+
+
+//  4.  What is the value of the alt atrribute on the image?
+//  5.  What is the dimensions and the url of the image?
