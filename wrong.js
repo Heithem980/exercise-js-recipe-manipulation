14e8a25e74722a92c61cf5f377a9651a4a016c99
Heithem980 <heithem.gha98@gmail.com>
2024-07-05 20:07:35 +0200

    part 2 - 6

---
+++ wrong.js
@@ -0,0 +1,56 @@
+//  -------------------------------------- PART 2 ------------------------------------------
+
+
+//  1.  The logo text of the site has the wrong color. Change it to the correct one.
+
+    const logoText = document.querySelector(".logo-text")
+
+    logoText.style.color = '#384241'
+
+
+
+
+//  2.  The alignment of the elements inside the header element are wrong. Change it to the correct 
+//      one. Hint, check the flex properties for the correct alignment.
+
+
+        const header = document.querySelector("header")
+
+        header.style.justifyContent = "flex-start"
+
+
+
+//  3.  The header has a border at the bottom, but it has the wrong color. Change it do the correct one.
+
+
+        header.style.borderBottom = "1px solid lightgrey"
+
+
+
+//  4.  The recipe name is wrong, change it to the correct one.
+
+    
+    const recipeName = document.getElementById("recipe-name");
+
+    recipeName.innerText = "Frozen Cheescake"
+
+
+
+//  5.  The clock icon beneath the recipe name has disappeared and been replaced by a text instead. 
+//      This can be fixed by adding a class to that element.
+
+
+
+
+
+
+
+
+//  6.
+//  7.
+//  8.
+//  9.
+// 10.
+// 11.
+// 12.
+// 13.
