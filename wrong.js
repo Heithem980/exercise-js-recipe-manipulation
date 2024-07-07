675eb9d0c9968309068af7179ea8fd627fd813f9
Heithem980 <heithem.gha98@gmail.com>
2024-07-07 13:49:56 +0200

    ++

--- wrong.js
+++ wrong.js
@@ -67,17 +67,59 @@
 
 //  8.  The background color of the ingredients list container is wrong. Fix it.
 
+
+    const ingredientsContainer = document.querySelector(".ingredients-container")
+    
+    ingredientsContainer.style.backgroundColor = "#f9f9f9"
+
 //  9.  The ingredients are divided in to two parts, one for the bottom and one for the paste. 
 //      In the list of the ingredients to the bottom, there is a text instead of two list items.
 //      Remove the text and add those two list items.
 
+    const listBottom = document.querySelector(".ingredients-list-bottom")
+
+    const childrenBottom = listBottom.children
+
+    listBottom.removeChild(childrenBottom[0])
+
+    listBottom.innerHTML = `<li>${"15st digistivetex"}</li>` + `<li>${"Lite smör"}</li>`
+
+
+
 // 10.  The third ingredient in the list of ingredients to the paste is wrong. 
 //      Change that specific ingredient to the correct one.
+
+    const listPaste = document.querySelector(".ingredients-list-paste")
+    const childrenPaste = listPaste.children
+    const childReplace = childrenPaste[2]
+
+    const newListElement  = document.createElement("li")
+    newListElement.innerText = "3tsk vaniljsocker"
+
+    listPaste.replaceChild(newListElement, childReplace)
+
 
 // 11.  There is also a missing ingredient in the list of ingredients to the paste. 
 //      Look and see what it is and add that one the the end of the list.
 
+    const newListElement2  = document.createElement("li")
+    const node = document.createTextNode("400g naturell philadelphiaost");
+
+    //console.log(newListElement2)
+    newListElement2.appendChild(node)
+
+    listPaste.appendChild(newListElement2)
+
+
 // 12.  The text "Instructions" to the left, beneath the image, has some shadow styling applied to it. 
 //      Remove that styling.
+
+//const element = document.querySelector(".instructions.shadow")
+
+//styles = window.getComputedStyle(element)
+//console.log(styles.boxshadow);
+//element.style.removeProperty("box-shadow")
+
+//console.log(element.innerHTML)
 
 // 13.  Two list elements of the list of instructions are incorrect. Find them and change them to the correct ones.
